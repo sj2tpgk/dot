14cfe26a70b3f52c0ea265b9abd8972ffb6e0377
sj2tpgk <github.fQ1wY8Wc.a87EgLB0@gmail.com>
2021-11-12 03:04:40 +0900

    misc

--- home-g/.uim.d/customs/custom-chewing-keys1.scm
+++
@@ -1,4 +0,0 @@
-(define chewing-on-key '("<Control> " "zenkaku-hankaku"))
-(define chewing-on-key? (make-key-predicate '("<Control> " "zenkaku-hankaku")))
-(define chewing-off-key '("<Control> " "zenkaku-hankaku"))
-(define chewing-off-key? (make-key-predicate '("<Control> " "zenkaku-hankaku")))
