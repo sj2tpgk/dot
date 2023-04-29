9ce98912e3435e58635baa7aa689f5edb85e5bc4
a <a>
2023-04-29 21:14:38 +0900

    misc

--- home/.config/nvim/init.lua
+++ home/.config/nvim/init.lua
@@ -777,7 +777,7 @@ fu! MyHighlight()
 endfu
 fu! MyHighlight2()
     hi      Normal                ctermfg=white   cterm=none
-    hi      Bold                  ctermfg=white   cterm=none
+    hi      Bold                  ctermfg=green   cterm=none
     hi      Comment               ctermfg=blue    cterm=none
     hi      String                ctermfg=yellow  cterm=none
     hi      Number                ctermfg=white   cterm=none
@@ -786,7 +786,7 @@ fu! MyHighlight2()
     hi      Type                  ctermfg=white   cterm=none
     hi      Flow                  ctermfg=red     cterm=none
     hi      Special               ctermfg=red     cterm=none
-    hi      PreProc               ctermfg=white   cterm=none
+    hi      PreProc               ctermfg=green   cterm=none
 
     hi link @constructor          FuncDef
     hi link @functiondef          FuncDef
@@ -1460,7 +1460,6 @@ if can_require"lspconfig" then -- ElDoc (lsp signatureHelp) <<<
             local sigLbl = sig.label
             local params = sig.parameters
             local actPar = sig.activeParameter or result.activeParameter
-            local s = 1
             lspsig = "ERROR!"
 
             if #params == 0 or #params <= actPar then -- no params (functions with no args etc.), or active param index is more than param count
@@ -1469,37 +1468,112 @@ if can_require"lspconfig" then -- ElDoc (lsp signatureHelp) <<<
                 return
             end
 
-            if type(params[1+actPar].label) == 'table' then -- param label is not string
-                local lbl = params[1+actPar].label -- { start, end }
-                local chunks = {
-                    { " ", "None" },
-                    { sigLbl:sub(1, lbl[1]),        "LspSig"    },
-                    { sigLbl:sub(lbl[1]+1, lbl[2]), "LspSigCur" },
-                    { sigLbl:sub(lbl[2]+1),         "LspSig"    },
-                }
-                nvim_echo_no_hitenter(chunks, true, {})
-                return
+            local toks = {} -- list of { type ("prefix"|"param"|"paramAct"|"delim"|"suffix"), text }
+            local tok
+            local function addTok(tok) table.insert(toks, tok) end
+
+            -- analyze signatures and construct toks
+            if type(params[1+actPar].label) == 'table' then -- param label is not string (e.g. pyright)
+
+                addTok({ "prefix", sigLbl:sub(1, params[1].label[1]) })
+                for i, par in ipairs(params) do
+                    if i >= 2 then
+                        addTok({ "delim", sigLbl:sub(params[i-1].label[2]+1, par.label[1]) })
+                    end
+                    addTok({ i == 1+actPar and "paramAct" or "param", sigLbl:sub(par.label[1]+1, par.label[2]) })
+                end
+                addTok({ "suffix", sigLbl:sub(params[#params].label[2]+1) })
+
+                -- local lbl = params[1+actPar].label -- { start, end }
+                -- mySigPrefix =
+                -- local chunks = {
+                --     { " ", "None" },
+                --     { sigLbl:sub(1, lbl[1]),        "LspSig"    },
+                --     { sigLbl:sub(lbl[1]+1, lbl[2]), "LspSigCur" },
+                --     { sigLbl:sub(lbl[2]+1),         "LspSig"    },
+                -- }
+                -- nvim_echo_no_hitenter(chunks, true, {})
+                -- return
+                --
+
+            else -- param label is string (e.g. tsserver)
+
+                local s = 1
+                local sePrev
+                for i = 1, #params do
+                    local parLbl = params[i].label
+                    local ss, se = sigLbl:find(parLbl, s, true) -- find parLbl in sigLbl
+                    if not ss then error("param " .. parLbl .. " not found") end
+                    if i == 1 then
+                        addTok({ "prefix", sigLbl:sub(1, ss-1) })
+                    else
+                        addTok({ "delim", sigLbl:sub(sePrev+1, ss-1) })
+                    end
+                    addTok({ i == 1+actPar and "paramAct" or "param", parLbl })
+                    if i == #params then
+                        addTok({ "suffix", sigLbl:sub(se+1) })
+                    end
+                    s = ss
+                    sePrev = se
+                end
+
+                -- local s = 1
+                -- for i = 1, #params do
+                --     local parLbl = params[i].label
+                --     local ss, se = sigLbl:find(parLbl, s, true) -- find parLbl in sigLbl
+                --     if not ss then error("param " .. parLbl .. " not found") end
+                --     if i == 1 + actPar then
+                --         -- update lspsig
+                --         lspsig = "%#LspSig# " .. sigLbl:sub(1, ss-1) .. "%#LspSigCur#" .. parLbl .. "%#LspSig#" .. sigLbl:sub(se+1) .. " %#None#"
+                --         -- show
+                --         local chunks = {
+                --             { " ",                 "None"      },
+                --             { sigLbl:sub(1, ss-1), "LspSig"    },
+                --             { parLbl,              "LspSigCur" },
+                --             { sigLbl:sub(se+1),    "LspSig"    },
+                --         }
+                --         nvim_echo_no_hitenter(chunks, true, {})
+                --         return
+                --     end
+                --     s = ss
+                -- end
+
             end
 
-            for i = 1, #params do
-                local parLbl = params[i].label
-                local ss, se = sigLbl:find(parLbl, s, true) -- find parLbl in sigLbl
-                if not ss then error("param " .. parLbl .. " not found") end
-                if i == 1 + actPar then
-                    -- update lspsig
-                    lspsig = "%#LspSig# " .. sigLbl:sub(1, ss-1) .. "%#LspSigCur#" .. parLbl .. "%#LspSig#" .. sigLbl:sub(se+1) .. " %#None#"
-                    -- show
-                    local chunks = {
-                        { " ",                 "None"      },
-                        { sigLbl:sub(1, ss-1), "LspSig"    },
-                        { parLbl,              "LspSigCur" },
-                        { sigLbl:sub(se+1),    "LspSig"    },
-                    }
-                    nvim_echo_no_hitenter(chunks, true, {})
-                    return
+            toks1 = toks
+            -- if sigLbl is too long, omit type signatures (from last params until sigLbl is sufficiently short)
+            local maxw = (vim.fn.winwidth(0) - 12) + 5
+            local sigLblLen = sigLbl:len()
+            for i = #toks, 1, -1 do
+                if sigLblLen < maxw then break end
+                local tok = toks[i]
+                local type = tok[1]
+                local text = tok[2]
+                if type == "param" or type == "paramAct" then
+                    local colonIdx = text:find(":")
+                    if colonIdx then -- note: colonIdx may be nil if param does not have type signature
+                        tok[2] = text:sub(1, colonIdx - 1)
+                        sigLblLen = sigLblLen - (text:len() - colonIdx + 1)
+                    end
                 end
-                s = ss
             end
+
+            -- convert toks to chunks and echo
+            local chunks = { { " ", "None" } }
+            for i, tok in ipairs(toks) do
+                local type = tok[1]
+                if type == "prefix" or type == "delim" or type == "suffix" then
+                    table.insert(chunks, { tok[2], "None" })
+                elseif type == "param" then
+                    table.insert(chunks, { tok[2], "LspSig" })
+                elseif type == "paramAct" then
+                    table.insert(chunks, { tok[2], "LspSigCur" })
+                else
+                    error("invalid type", type)
+                end
+            end
+
+            nvim_echo_no_hitenter(chunks, true, {})
 
             -- exmple value of "result"
             -- {
