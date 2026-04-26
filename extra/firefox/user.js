27fee177827f13fa2f99fffeefc5d088341ac55d
a <a>
2026-04-26 10:40:16 +0900

    misc

--- extra/firefox/user.js
+++ extra/firefox/user.js
@@ -4,6 +4,7 @@
 // (Settings will be saved in prefs.js)
 
 user_pref("accessibility.blockautorefresh", true); // note: uMatrix "Spoof <noscript> tags" must be DISABLED to block <meta http-equiv="refresh"> inside <noscript>
+user_pref("apz.gtk.kinetic_scroll.enabled", false); // "false" = scroll stop immediately when you leave you finger
 user_pref("browser.aboutConfig.showWarning", false);
 user_pref("browser.compactmode.show", true);
 user_pref("browser.display.use_document_fonts", 1); // remote font: 1 = enable (you can "Block remote fonts" in uBO config page and only allow on certain sites via its config panel; however this is not suitable when uBO is not installed. note: when remote font is disabled some PDFs will not render properly (ms office web too if I remember correctly))
